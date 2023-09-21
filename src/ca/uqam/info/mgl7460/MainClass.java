a725a13d6ff88339b8706a77046331547557a7bf
hafedhmili <mili.hafedh@uqam.ca>
2023-09-21 11:57:52 -0400

    Coquille vide

---
+++ src/ca/uqam/info/mgl7460/MainClass.java
@@ -0,0 +1,11 @@
+package ca.uqam.info.mgl7460;
+
+public class MainClass
+{
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hello, MGL7460 World!");
+        
+    }
+}
